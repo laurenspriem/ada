d1541d400b76b3d7e161b88ca4b0a5a3321d1f48
Lisannnnnn <lisangraumans@gmail.com>
2022-05-13 21:28:41 +0200

    added accounts
    -new template
    -sub table don't work yet

---
+++ services/account/src/account/app.py
@@ -0,0 +1,156 @@
+import os
+
+from flask import Flask
+from huey import MemoryHuey
+from google.cloud import pubsub
+from sqlalchemy import create_engine
+from sqlalchemy.engine.url import URL
+from sqlalchemy.orm import scoped_session, sessionmaker
+from werkzeug.exceptions import HTTPException
+
+from account.repositories import (
+    UserDatabaseRepository,
+    ExamplePubSubRepository,
+    ExampleWebRepository,
+)
+from account.resources import resources
+from account.web import WebClient
+from account.db import Base
+
+
+def _connect_db():
+    # Load config
+    db_host = os.getenv("DB_HOST")
+    db_port = os.getenv("DB_PORT")
+    db_user = os.getenv("DB_USER")
+    db_password = os.getenv("DB_PASSWORD")
+    db_socket = os.getenv("DB_SOCKET")
+    db_instance = os.getenv("DB_INSTANCE")
+    db_name = os.getenv("DB_NAME")
+
+    # Create uri
+    uri = URL.create(
+        drivername="postgresql+pg8000",
+        host=db_host,
+        port=db_port,
+        username=db_user,
+        password=db_password,
+        database=db_name,
+        query={"unix_sock": f"{db_socket}/{db_instance}/.s.PGSQL.5432"}
+        if db_socket and db_instance
+        else {},
+    )
+
+    # Create engine
+    engine = create_engine(uri)
+
+    # Create session
+    session = scoped_session(
+        sessionmaker(
+            autocommit=False,
+            autoflush=False,
+            bind=engine,
+        )
+    )
+
+    # Create tables
+    Base.metadata.create_all(bind=engine)
+
+    return session
+
+
+def _connect_pubsub():
+    # Load config
+    pubsub_project_id = os.getenv("PUBSUB_PROJECT_ID")
+
+    # Create client
+    publisher = pubsub.PublisherClient()
+    subscriber = pubsub.SubscriberClient()
+
+    return pubsub_project_id, publisher, subscriber
+
+
+def _connect_web():
+    # Create client
+    client = WebClient()
+
+    return client
+
+
+def create_app():
+    # Create app
+    flask = Flask(__name__)
+
+    # Register resources
+    flask.register_blueprint(resources)
+
+    # Register health
+    @flask.route("/api/health/")
+    def default_health():
+        return {"status": "OK"}
+
+    # Register errors
+    @flask.errorhandler(HTTPException)
+    def default_handler(error):
+        return {"message": error.name}, error.code
+
+    # Create database
+    session = _connect_db()
+
+    # Create pub/sub
+    project_id, publisher, subscriber = _connect_pubsub()
+
+    # Create web
+    client = _connect_web()
+
+    # Set repositories
+    setattr(
+        flask,
+        "repositories",
+        {
+            "database_repository": UserDatabaseRepository(session),
+            "pubsub_repository": ExamplePubSubRepository(
+                project_id,
+                publisher,
+                subscriber,
+            ),
+            "web_repository": ExampleWebRepository(client),
+        },
+    )
+
+    return flask
+
+
+def create_worker():
+    # Create worker
+    huey = MemoryHuey()
+
+    # Create database
+    session = _connect_db()
+
+    # Create pub/sub
+    project, publisher, subscriber = _connect_pubsub()
+
+    # Create web
+    client = _connect_web()
+
+    # Set subscriptions
+    setattr(
+        huey,
+        "repositories",
+        {
+            "database_repository": UserDatabaseRepository(session),
+            "pubsub_repository": ExamplePubSubRepository(
+                project,
+                publisher,
+                subscriber,
+            ),
+            "web_repository": ExampleWebRepository(client),
+        },
+    )
+
+    return huey
+
+
+app = create_app()
+worker = create_worker()
---
+++ services/account/src/account/repositories.py
@@ -0,0 +1,154 @@
+import json
+
+from google.api_core.exceptions import AlreadyExists
+
+from account.models import User, Profile, NotificationSettings, PaymentInfo, ShippingInfo
+
+
+class UserDatabaseRepository:
+    def __init__(self, session):
+        self._session = session
+
+    def create_user(self, user):
+        self._session.add(user)
+        self._session.commit()
+        self._session.refresh(user)
+        return user
+
+    def get_user(self, d_username):
+        user = self._session.query(User).filter(User.username == d_username).first()
+        return user
+
+    def update_user(self, d_username, d_email, d_password):
+        user = self.get_user(d_username)
+        user.email = d_email
+        user.password = d_password
+        self._session.commit()
+        return user
+
+    def delete_user(self, d_username):
+        self._session.query(User).filter(User.username == d_username).delete()
+        self._session.commit()
+        return d_username
+
+    def update_profile(self, d_username, d_birthday, d_gender, d_height, d_shirt_size, d_jeans_size, d_shoe_size):
+        user = self.get_user(d_username)
+        user.profile.birthday = d_birthday
+        user.profile.gender = d_gender
+        user.profile.height = d_height
+        user.profile.shirt_size = d_shirt_size
+        user.profile.jeans_size = d_jeans_size
+        user.profile.shoe_size = d_shoe_size
+        self._session.commit()
+        return user
+
+    def update_shippinginfo(self, d_username, d_street, d_street_number, d_zip_code, d_city):
+        user = self.get_user(d_username)
+        user.shippinginfo.street = d_street
+        user.shippinginfo.street_number = d_street_number
+        user.shippinginfo.zip_code = d_zip_code
+        user.shippinginfo.city = d_city
+        self._session.commit()
+        return user
+
+    def update_notificationsettings(self, d_username, d_item_notifications_enabled, d_bids_notifications_enabled, d_chat_notifications_enabled, d_news_notifications_enabled):
+        user = self.get_user(d_username)
+        user.notificationsettings.item_notifications_enabled = d_item_notifications_enabled
+        user.notificationsettings.bids_notifications_enabled = d_bids_notifications_enabled
+        user.notificationsettings.chat_notifications_enabled = d_chat_notifications_enabled
+        user.notificationsettings.news_notifications_enabled = d_news_notifications_enabled
+        self._session.commit()
+        return user
+
+    def update_paymentinfo(self, d_username, d_iban, d_name, d_bank):
+        user = self.get_user(d_username)
+        user.paymentinfo.iban = d_iban
+        user.paymentinfo.name = d_name
+        user.paymentinfo.bank = d_bank
+        self._session.commit()
+        return user
+
+
+
+
+class ExamplePubSubRepository:
+    TEST_TOPIC = "test"
+
+    def __init__(self, project_id, publisher, subscriber):
+        self._project_id = project_id
+        self._publisher = publisher
+        self._subscriber = subscriber
+
+    def pull(self, topic):
+        topic_path = self._ensure_topic_exists(topic)
+        subscription_path = self._ensure_subscription_exists(topic_path)
+
+        res = self._subscriber.pull(
+            subscription=subscription_path,
+            return_immediately=True,
+            max_messages=1,
+        )
+
+        if len(res.received_messages) == 0:
+            return []
+
+        received_message_ids = [m.ack_id for m in res.received_messages]
+        received_message_data = [
+            json.loads(m.message.data.decode("utf-8")) for m in res.received_messages
+        ]
+
+        self._subscriber.acknowledge(
+            subscription=subscription_path,
+            ack_ids=received_message_ids,
+        )
+
+        return received_message_data
+
+    def push(self, topic, message):
+        topic_path = self._ensure_topic_exists(topic)
+
+        res = self._publisher.publish(topic_path, str.encode(json.dumps(message)))
+
+        return {"message_id": res.result()}
+
+    def _ensure_topic_exists(self, topic):
+        topic_path = self._publisher.topic_path(self._project_id, topic)
+
+        try:
+            self._publisher.create_topic(name=topic_path)
+        except AlreadyExists:
+            pass
+
+        return topic_path
+
+    def _ensure_subscription_exists(self, topic_path):
+        subscription_path = self._subscriber.subscription_path(
+            self._project_id,
+            f"{topic_path[topic_path.rindex('/')+1:]}-subscription",
+        )
+
+        try:
+            self._subscriber.create_subscription(
+                name=subscription_path,
+                topic=topic_path,
+            )
+        except AlreadyExists:
+            pass
+
+        return subscription_path
+
+
+class ExampleWebRepository:
+    TEST_SERVICE = "http://test:8080/api"
+
+    def __init__(self, client):
+        self._client = client
+
+    def get(self, endpoint):
+        return self._client.get(endpoint)
+
+    def post(self, endpoint, data):
+        return self._client.post(endpoint, data=data)
+
+    def put(self, endpoint, data):
+        return self._client.put(endpoint, data=data)
