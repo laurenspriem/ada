c9d23cc1ae224dc014b62dd7e690cd68599ee8b5
Lisannnnnn <lisangraumans@gmail.com>
2022-05-13 10:37:24 +0200

    Bidding check

--- services/bidding/daos/bid_dao.py
+++ services/bidding/daos/bid_dao.py
@@ -1,8 +1,9 @@
 from sqlalchemy import Column, String, Integer, DateTime, ForeignKey, Float, Boolean
 from db import Base
-from datetime import datetime
+from datetime import date
 
-class BIDDAO(Base):
+
+class BidDAO(Base):
     __tablename__ = 'Bidding'
     id = Column(Integer, primary_key=True, autoincrement=True) 
     price = Column(Float)
@@ -12,8 +13,7 @@ class BIDDAO(Base):
     bid_accepted = Column(Boolean)
     price_accepted = Column(Boolean)
 
-    def __init__(self, state, price, status, date_created, date_updated, bid_accepted=True, price_accepted=True):
-        self.state = state
+    def __init__(self, price, status, date_created=date.today(), date_updated=date.today(), bid_accepted=False, price_accepted=False):
         self.price = price
         self.status = status
         self.date_created = date_created    # Is set to current time in item.py
--- services/bidding/resources/Bid.py
+++ services/bidding/resources/Bid.py
@@ -1,7 +1,7 @@
 from matplotlib.pyplot import title
 from flask import jsonify
 from db import Session
-from daos.bid_dao import BIDDAO
+from daos.bid_dao import BidDAO
 
 from datetime import datetime
 from cmath import nan
@@ -9,54 +9,53 @@ from cmath import nan
 
 class Bid:
     @staticmethod
-    def create(bid_id):
+    def create(body):
         session = Session()
-        bid_id = BIDDAO(Bid['price'], datetime.now(), datetime.now()) # klopt dit dat deze gelinkt is aan de item_id?
         # Add here the condition of the bid must be higher than the previous bid?
-        # Add here that the bid must be higher than the minimum assigned value of a pro
-        bid_id.add(bid_id)
-        bid_id.commit()
-        bid_id.refresh(bid_id)
-        bid_id.close()
-        return jsonify({'item_id': Bid.id}, bid_id), 200
+        # Add here that the bid must be higher than the minimum assigned value of a product
+        bid = BidDAO(body['price'], body['status'])
+        session.add(bid)
+        session.commit()
+        session.refresh(bid)
+        session.close()
+        return jsonify({'item_id': bid.id}), 200
         
     @staticmethod
-    def update(bid_id):
+    def update(d_bid_id, body):
         session = Session()
-        bid_id = session.query(BIDDAO).filter(BIDDAO.id == bid_id).first()
-        bid_id.price = Bid['price']
-        bid_id.status = Bid['status']
-        bid_id.date_updated = datetime.now()
+        bid = session.query(BidDAO).filter(BidDAO.id == d_bid_id)
+        bid.price = body['price']
+        bid.status = body['status']
+        bid.date_updated = datetime.now()
         session.commit()
-        return jsonify({'message': f'The bid price with id {bid_id} is updated'}), 200
+        return jsonify({'message': f'The bid with id {d_bid_id} is updated'}), 200
 
     @staticmethod
-    def get(bid_id):
+    def get(d_bid_id):
         session = Session()
-        bid = session.query(BIDDAO).filter(BIDDAO.id == bid_id).first()
+        bid = session.query(BidDAO).filter(BidDAO.id == d_bid_id).first()
 
         if bid:
-            status_obj = bid_id.status
             text_out = {
-                "item_id": bid_id.id,
-                "prize": bid_id.price,
-                "status": bid_id.status,
-                "date_created": bid_id.date_created,
-                "date_created": bid_id.date_updated,
+                "bid_id": bid.id,
+                "prize": bid.price,
+                "status": bid.status,
+                "date_created": bid.date_created,
+                "date_created": bid.date_updated,
             }
             session.close()
             return jsonify(text_out), 200
         else:
             session.close()
-            return jsonify({'message': f'There is no bid for this item yet {bid_id}'}), 404
+            return jsonify({'message': f'There is no bid for this item yet {d_bid_id}'}), 404
 
     @staticmethod
-    def delete(bid_id):
+    def delete(d_bid_id):
         session = Session()
-        effected_rows = session.query(BIDDAO).filter(BIDDAO.id == bid_id).delete()
+        effected_rows = session.query(BidDAO).filter(BidDAO.id == d_bid_id).delete()
         session.commit()
         session.close()
         if effected_rows == 0:
-            return jsonify({'message': f'There is no bid with id {bid_id}'}), 404
+            return jsonify({'message': f'There is no bid with id {d_bid_id}'}), 404
         else:
             return jsonify({'message': 'The bid is removed'}), 200
