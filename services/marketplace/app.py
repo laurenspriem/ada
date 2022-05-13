a0ae87e159cd62be3d6176f8019a42462adb1a1c
laurenspriem <github_uni.fishers@aleeas.com>
2022-05-13 15:14:57 +0200

    Update Marketplace service

--- services/marketplace/app.py
+++ services/marketplace/app.py
@@ -5,6 +5,7 @@ from resources.item import Item
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
+app.config['JSON_SORT_KEYS'] = False
 Base.metadata.create_all(engine)
 
 
@@ -28,6 +29,14 @@ def update_item(item_id):
 @app.route('/items/<item_id>', methods=['DELETE'])
 def delete_item(item_id):
     return Item.delete(item_id)
+
+@app.route('/items/list/<user_id>', methods=['GET'])
+def get_list(user_id):
+    return Item.getlist(user_id)
+
+@app.route('/items/search/<keyword>', methods=['GET'])
+def search(keyword):
+    return Item.search(keyword)
 
 
 app.run(host='0.0.0.0', port=5000)
