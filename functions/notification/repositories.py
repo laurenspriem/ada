14d9401f2f576a90116989929a9caa7ec153af2d
tomdewildt <32359630+tomdewildt@users.noreply.github.com>
2022-05-18 14:05:35 +0200

    Replace logging for print

--- functions/notification/repositories.py
+++ functions/notification/repositories.py
@@ -1,11 +1,6 @@
-import logging
-
-log = logging.getLogger(__name__)
-
-
 class NotificationPrintRepository:
     def __init__(self):
         pass
 
     def send_notification(self, message):
-        log.info(message)
+        print(message)
