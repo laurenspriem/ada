80b3a62522453d5062d7ecc1d13c5b0554133517
Tom de Wildt <32359630+tomdewildt@users.noreply.github.com>
2022-05-18 12:11:24 +0200

    Add event bus communication to bidding service (#62)

--- services/bidding/src/bidding/interactions.py
+++ services/bidding/src/bidding/interactions.py
@@ -6,28 +6,49 @@ class BiddingInteractions:
         self._database_repository = repositories["database_repository"]
         self._pubsub_repository = repositories["pubsub_repository"]
 
-    def list(self):
-        return [m.to_dict() for m in self._database_repository.list_models()]
-
     def create_bid(self, data):
         bid = Bid(
+            item_id=data["item_id"],
+            user_id=data["user_id"],
             price=data["price"],
             status=data["status"],
             bid_accepted=data["bid_accepted"],
             price_accepted=data["price_accepted"],
         )
 
-        return self._database_repository.create_bid(bid).to_dict()
+        bid = self._database_repository.create_bid(bid).to_dict()
+        if bid["bid_accepted"]:
+            self._pubsub_repository.push(
+                self._pubsub_repository.OFFER_ACCEPTED_TOPIC,
+                bid,
+            )
+
+        return bid
 
     def get_bid(self, data):
         return self._database_repository.get_bid(int(data)).to_dict()
 
     def update_bid(self, id, data):
-        price=data["price"]
-        status=data["status"]
-        bid_accepted=data["bid_accepted"]
-        price_accepted=data["price_accepted"]
-        return self._database_repository.update_bid(int(id), price, status, bid_accepted, price_accepted)
+        price = data["price"]
+        status = data["status"]
+        bid_accepted = data["bid_accepted"]
+        price_accepted = data["price_accepted"]
+
+        bid = self._database_repository.update_bid(
+            int(id),
+            price,
+            status,
+            bid_accepted,
+            price_accepted,
+        ).to_dict()
+
+        if bid["bid_accepted"]:
+            self._pubsub_repository.push(
+                self._pubsub_repository.OFFER_ACCEPTED_TOPIC,
+                bid,
+            )
+
+        return bid
 
     def delete_bid(self, d_id):
         return self._database_repository.delete_bid(int(d_id))
--- services/bidding/src/bidding/models.py
+++ services/bidding/src/bidding/models.py
@@ -9,6 +9,8 @@ class Bid(Base):
     id = sa.Column(sa.Integer, primary_key=True, autoincrement=True)
     price = sa.Column(sa.Integer, nullable=False)
     status = sa.Column(sa.String, nullable=False)
+    item_id = sa.Column(sa.Integer, nullable=False)
+    user_id = sa.Column(sa.Integer, nullable=False)
     bid_accepted = sa.Column(sa.Boolean, nullable=False)
     price_accepted = sa.Column(sa.Boolean, nullable=False)
     created_on = sa.Column(
@@ -26,6 +28,8 @@ class Bid(Base):
             "id": self.id,
             "price": self.price,
             "status": self.status,
+            "item_id": self.item_id,
+            "user_id": self.user_id,
             "bid_accepted": self.bid_accepted,
             "price_accepted": self.price_accepted,
             "created_on": self.created_on.isoformat(),
