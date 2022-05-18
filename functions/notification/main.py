b20b4b366bd2fa61332e1a39925517eeb6b6ac44
tomdewildt <32359630+tomdewildt@users.noreply.github.com>
2022-05-18 14:48:22 +0200

    Update notification function

--- functions/notification/main.py
+++ functions/notification/main.py
@@ -17,7 +17,34 @@ def _connect_notification():
         notification_repository = NotificationPrintRepository()
 
 
-def pull_topic(event, context):
+def pull_item_update_topic(event, context):
+    interactions = NotificationInteractions(notification_repository)
+
+    topic = context.event_id
+    data = json.loads(base64.b64decode(event["data"]).decode("utf-8"))
+
+    return interactions.send_notification(topic, data)
+
+
+def pull_message_send_topic(event, context):
+    interactions = NotificationInteractions(notification_repository)
+
+    topic = context.event_id
+    data = json.loads(base64.b64decode(event["data"]).decode("utf-8"))
+
+    return interactions.send_notification(topic, data)
+
+
+def pull_offer_accepted_topic(event, context):
+    interactions = NotificationInteractions(notification_repository)
+
+    topic = context.event_id
+    data = json.loads(base64.b64decode(event["data"]).decode("utf-8"))
+
+    return interactions.send_notification(topic, data)
+
+
+def pull_user_blocked_topic(event, context):
     interactions = NotificationInteractions(notification_repository)
 
     topic = context.event_id
